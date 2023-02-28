bdce25a48c47fff4f8f41261d45a87c6401d3fe7
Ariel <kshhj2118@gmail.com>
2023-02-28 21:41:31 +0900

    0228 전화번호 목록 solved

---
+++ 김소현/0228_전화번호 목록.py
@@ -0,0 +1,18 @@
+phone_book1 = ["119","97674223","1195524421"]
+phone_book2 = ["123","456","789"]
+phone_book3 = ["12","123","1235","567","88"]
+
+def solution(phone_book):
+    phone_book.sort()
+    len1 = len(phone_book)
+
+    for i in range(1,len1):
+        if phone_book[i].startswith(phone_book[0]):
+            return False
+        else:
+            return True
+
+    
+print(solution(phone_book1))
+print(solution(phone_book2))
+print(solution(phone_book3))
